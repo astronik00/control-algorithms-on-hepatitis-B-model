e46c4478440e95607d43c77ac91d87942c4e96ef
astronik00 <astronik00@gmail.com>
2023-08-01 15:51:05 +0300

    Added discrete plot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import utils.plotter as plotter
-from scheme.continuous import calculate
+import scheme.discrete as discrete
+import scheme.continuous as continious
 
 filepath = 'coefficients/coeffs1.txt'
 a = [float(x) for x in open(filepath).read().split("\n")]
@@ -12,7 +13,14 @@ control_labels = ["\psi", "\psi_1", "\psi_2", "u"]
 # df = plotter.to_df(t, x, control, [model_labels, control_labels])
 # plotter.plot_x(df, 'images/no_control/')
 
-t, x, control = calculate([0, 150, 0.01], history, a,
-                          control_params={'type': 'adar', 'x1_const': 1e-8, 'T1': 0.7, 'T2': 1.0})
+# adar+dadar
+
+t, x, control = continious.calculate([0, 150, 0.01], history, a,
+                                     control_params={'type': 'adar', 'x1_const': 1e-8, 'T1': 0.7, 'T2': 1.0})
 df1 = plotter.to_df(t, x, control, [model_labels, control_labels])
-plotter.plot_x(df1, 'images/adar/')
+
+t, x, control = discrete.calculate([0, 150, 0.01], history, a,
+                                   control_params={'type': 'adar', 'x1_const': 1e-8, 'l1': -0.1, 'l2': -0.2})
+df2 = plotter.to_df(t, x, control, [model_labels, control_labels])
+
+plotter.plot_two(df1, df2, 'images/adar+dadar/')
